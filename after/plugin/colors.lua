f91fe7bd9e925364aa63d8aed5ab984e2907bd53
makz mann <mr.greenmousth@gmail.com>
2022-11-26 19:45:29 -0300

    colors, deletions and more

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -3,15 +3,15 @@ require('tokyonight').setup({
     transparent = true,
     styles = {
         sidebars = 'transparent',
-        -- floats = 'transparent',
     },
-    -- sidebars = { 'help' },
     on_highlights = function(hi, colors) 
         hi.LineNr = { fg = colors.orange }
         hi.LineNrAbove = { fg = colors.blue }
         hi.LineNrBelow = { fg = colors.blue }
         hi.Winbar = { fg = colors.orange }
         hi.WinbarNC = { fg = colors.blue }
+        hi.ColorColumn = { bg = '#222222' }
+        hi.StatusLine = { bg = colors.orange, fg = colors.black }
     end,
 })
 
