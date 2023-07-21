fb062535190377a5e2fceff75034edd98a9cf9c9
Makz Mann <mr.greenmousth@gmail.com>
2023-07-21 19:29:13 -0400

    wezterm initial config

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,17 @@
+local wezterm = require 'wezterm'
+
+local config = {}
+
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+config.color_scheme = 'Tokyo Night (Gogh)'
+config.font = wezterm.font 'Hermit'
+config.window_background_opacity = .98
+
+config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = true
+config.hide_tab_bar_if_only_one_tab = true
+
+return config
