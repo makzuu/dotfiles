01942fc24d126466ad76ce1b21dc22cfdf0cccc8
makzuu <mr.greenmousth@gmail.com>
2024-01-22 22:20:06 -0300

    Añade opciones basicas a la configuracion de neovim

---
+++ neovim/.config/nvim/init.lua
@@ -0,0 +1,27 @@
+-- :options
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+vim.opt.wrap = false
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.colorcolumn = "80"
+
+vim.opt.scrolloff = 8
+
+vim.opt.hlsearch = false
+
+vim.opt.ignorecase = true
+
+vim.opt.termguicolors = true
+
+vim.opt.guicursor = "a:block"
