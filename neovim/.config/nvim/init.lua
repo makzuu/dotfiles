70f9f4fc902099cc15d36b106bdfed7fa5f4d22b
makzuu <mr.greenmousth@gmail.com>
2024-01-22 22:49:07 -0300

    Añade un plugin manager y un colorscheme
    plugin manager: lazy.nvim
    colorscheme: rose-pine

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -25,3 +25,24 @@ vim.opt.ignorecase = true
 vim.opt.termguicolors = true
 
 vim.opt.guicursor = "a:block"
+
+-- lazy.nvim
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+    { 'rose-pine/neovim', name = 'rose-pine' }
+})
+
+vim.cmd.colorscheme("rose-pine")
