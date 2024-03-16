cbb6cb9d91324208ddb76d937f4478ed7b018964
makzuu <mr.greenmousth@gmail.com>
2024-03-16 17:25:44 -0300

    Add telescope plugin

--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -68,5 +68,26 @@ require("lazy").setup({
                 }
             })
         end
-    }
+    },
+    {
+        'nvim-telescope/telescope.nvim', tag = '0.1.6',
+        -- or                              , branch = '0.1.x',
+        dependencies = { 'nvim-lua/plenary.nvim' },
+        config = function()
+            require('telescope').setup({
+                defaults = {
+                    preview = false,
+                },
+                pickers = {
+                    find_files = { theme = "ivy", },
+                    live_grep = { theme = "ivy", },
+                },
+            })
+            local builtin = require('telescope.builtin')
+            vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+            vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+            vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+            vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+        end
+    },
 })
