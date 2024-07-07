a3800c5adf3a6a0b4d14a57e5be5aa7790ca9366
makzuu <mr.greenmousth@gmail.com>
2024-07-07 15:18:53 -0400

    Update wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,14 +1,17 @@
-local wezterm = require('wezterm')
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
 local config = wezterm.config_builder()
 
-config.font = wezterm.font('HurmitNerdFontMono')
-config.color_scheme = 'rose-pine'
-config.window_background_opacity = .98
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = "rose-pine"
 
 config.use_fancy_tab_bar = false
-config.tab_bar_at_bottom = true
 config.hide_tab_bar_if_only_one_tab = true
-
 config.force_reverse_video_cursor = true
 
+-- and finally, return the configuration to wezterm
 return config
