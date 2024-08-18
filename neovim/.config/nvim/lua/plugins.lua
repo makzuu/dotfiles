e2ec46881e8441cf6956fadb124af233d73c038c
makzuu <mr.greenmousth@gmail.com>
2024-08-18 13:20:44 -0400

    Update colorscheme

--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -13,10 +13,9 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
     {
-        "rose-pine/neovim",
-        name = "rose-pine",
-        config = function ()
-            vim.cmd.colorscheme("rose-pine")
+        "catppuccin/nvim", name = "catppuccin", priority = 1000,
+        config = function()
+            vim.cmd.colorscheme("catppuccin")
         end
     },
     {
--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -7,7 +7,7 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = "rose-pine"
+config.color_scheme = "Catppuccin Mocha"
 
 config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
