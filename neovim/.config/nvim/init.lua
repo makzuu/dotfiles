223a82554459f2ff7eba92db3b975db8534ca7a3
makzuu <mr.greenmousth@gmail.com>
2024-12-08 14:06:05 -0300

    (neovim) install treesitter

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -7,6 +7,8 @@ vim.opt.shiftwidth = 4
 vim.opt.number = true
 vim.opt.relativenumber = true
 
+vim.opt.clipboard = "unnamedplus"
+
 vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
 vim.keymap.set("n", "<space>x", "<cmd>.lua<CR>")
 vim.keymap.set("v", "<space>x", ":lua<CR>")
