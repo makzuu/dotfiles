83a8bffc5c54a7de9205dc09b45c5301edced71e
makzuu <mr.greenmousth@gmail.com>
2024-12-27 22:56:43 -0300

    (neovim) update config

--- neovim/.config/nvim/lua/config/lazy.lua
+++ neovim/.config/nvim/lua/config/lazy.lua
@@ -8,7 +8,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
+      { out,                            "WarningMsg" },
       { "\nPress any key to exit..." },
     }, true, {})
     vim.fn.getchar()
@@ -26,7 +26,7 @@ vim.g.maplocalleader = "\\"
 -- Setup lazy.nvim
 require("lazy").setup({
   spec = {
-    { "folke/tokyonight.nvim", config = function() vim.cmd.colorscheme("tokyonight") end },
+    { "folke/tokyonight.nvim",  config = function() vim.cmd.colorscheme("tokyonight-night") end },
     -- import your plugins
     { import = "config.plugins" },
   },
